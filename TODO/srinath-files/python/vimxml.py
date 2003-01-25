ac2fd31deb54cff1b8efc1c361a4a066dc855777
srinathava <srinathava@a797aaa2-510f-0410-87df-a343b97812ff>
2003-01-25 03:00:23 +0000

    *** empty log message ***
    git-svn-id: https://vim-latex.svn.sourceforge.net/svnroot/vim-latex/trunk@367 a797aaa2-510f-0410-87df-a343b97812ff

--- TODO/srinath-files/python/vimxml.py
+++ TODO/srinath-files/python/vimxml.py
@@ -1,8 +1,9 @@
-import textutils,string
-import numarray
-from textutils import GetTextFromElementNode
+from textutils import FormatTable,IndentParagraphs
+from domutils import GetTextFromElementNode
+import string
 
-def handleOptions(fileDom):
+def handleFile(fileDom):
+	text = ""
 	# for each child node of the document root...
 	for child in fileDom.documentElement.childNodes:
 		# if the child is an "element" and it has some handler function in
@@ -11,29 +12,73 @@ def handleOptions(fileDom):
 		if child.nodeType == child.ELEMENT_NODE:
 			tagName = child.tagName
 			if handlerMaps.has_key(tagName):
-				handlerMaps[tagName](child)
+				text += handlerMaps[tagName](child)
+
+		# if its a text node, then simply append.
+		elif child.nodeType == child.TEXT_NODE:
+			text += child.data
+
+
+def handleOptions(optionsNode):
+	options = optionsNode.getElementsByTagName("option")
+	
+	retText = ""
+	for option in options:
+		retText += handleOption(option)
+		retText += "\n" * vimformat.PAR_SPACE
+
+	return retText
+
 
 def handleOption(option):
+	retText = ""
 	names = GetTextFromElementNode(option, "name")
 
 	for name in names:
-		print string.rjust("*"+name+"*", 70)
+		retText += string.rjust("*"+name+"*", 70)
 
 	for name in names:
-		print "\n" + name,
+		retText += "\n" + name,
 
-	print "\n",
+	retText += "\n"
 	desc = GetTextFromElementNode(option, "desc")[0]
-	print textutils.IndentParagraphs(desc, 50, 20)
+	return IndentParagraphs(desc, 50, 20)
+
 
 def handleOptionDefault(default):
 	type = string.join(GetTextFromElementNode(default, "type"), "\n")
 	extra = string.join(GetTextFromElementNode(default, "extra"), "\n")
-	print type + "\t(" + extra + ")"
+	return type + "\t(" + extra + ")"
+
+
+def handleTable(root):
+	"""
+	dom_HandleTable(root)
+
+	Given a Element instance, handles the table inside of it.
+
+	NOTE: The document instance will be treated as a single table.
+		  Therefore, if a document contains multiple tables, then this
+		  function needs to be iterated over it.
+
+	      Also, nested tables are not handled.
+	"""
+	text = []
+	rows = root.getElementsByTagName("row")
+	for row in rows:
+		colTexts = GetTextFromElementNode(row, "col")
+		text.append(colTexts)
+
+	return FormatTable(text, COL_SPACE = 5)
 
 
 handlerMaps = {
+	'file': handleFile,
 	'options': handleOptions,
 	'option': handleOption,
 	'optionDefault': handleOptionDefault
+	'table': handleTable
 }
+if __name__ == '__main__':
+	xmlFile = open('text.xml', 'r')
+	xmlString = string.join(xmlFile.readlines(), "")
