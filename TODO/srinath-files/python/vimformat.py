c673d5ff9c97f2d54060aa8d046464eedf1525f1
srinathava <srinathava@a797aaa2-510f-0410-87df-a343b97812ff>
2003-01-27 20:02:49 +0000

    *** empty log message ***
    git-svn-id: https://vim-latex.svn.sourceforge.net/svnroot/vim-latex/trunk@369 a797aaa2-510f-0410-87df-a343b97812ff

---
+++ TODO/srinath-files/python/vimformat.py
@@ -0,0 +1,11 @@
+"""
+Just a bunch of constants defined for formatting
+"""
+
+PAR_SPACE = 3
+OPTION_WIDTH = 50
+OPTION_OFFSET = 20
+TEXT_WIDTH = 80
+ELEMENT_SPACE = "\n"
+BLOCK_QUOTE = "    "
+COL_SPACE = 5
