15ea1352e3843d61257dd6852fddcf890621a78e
srinathava <srinathava@a797aaa2-510f-0410-87df-a343b97812ff>
2003-11-06 07:40:02 +0000

    A somewhat improved structure of index.php. We rely on the calling URL's to form the subject and title.
    git-svn-id: https://vim-latex.svn.sourceforge.net/svnroot/vim-latex/trunk@783 a797aaa2-510f-0410-87df-a343b97812ff

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,51 +1,53 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<!--
+index.php generates the html page using <table>'s to create the layout.
+Essentially, the HTML page has the following structure:
+
+--------------------%<--------------------
+<table>
+  <tr>
+    <td>$navigation_panel</td>
+    <td>$main_panel</td>
+  </tr>
+  <tr>
+    <td colspan=2>$footnote_panel</td>
+  </tr>
+</table>
+--------------------%<--------------------
+
+In the above structure, the various elements are formed as follows:
+
+1. $navigation_panel: This is simply done by including head.inc.
+
+2. $main_panel: This is formed via the subject key in the query to this
+   page. For example, if we call index.php as
+   	 index.php?subject=coding-style
+   then it will $main_panel will simply include coding-style.inc.
+
+   If no subject key is specified, then we include main.inc.
+
+   In addition, if a title is specified in the query, then it is used to
+   form the title of the HTML page.
+
+3. $footnote_panel is simply foot.inc.
+
+-->
 <?
-if (isset($_GET['subject']))
-{
-	$subject = $_GET['subject'];
-	switch ($subject)
-	{
-		case 'packages':
-			$pagetitle="Package files";
-		 	break;
-		case 'screenshots':
-			$pagetitle="Screenshots";
-			break;
-		case 'features':
-			$pagetitle="Features";
-			break;
-		case 'templates':
-			$pagetitle="Templates";
-			break;
-		case 'links':
-			$pagetitle="Links";
-			break;
-		case 'weare':
-			$pagetitle="We are ...";
-			break;
-		case 'download':
-			$pagetitle="Download";
-			break;
-		case 'faq':
-			$pagetitle="FAQ";
-			break;
-		case 'comingsoon':
-			$pagetitle="Coming Soon ...";
-			break;
-	}
-	$leftpanel="left_".$subject.".inc";
-	$mainpanel=$subject.".inc";
+if (isset($_GET['subject'])) {
+	$mainpanel = $_GET['subject'].".inc";
 }
-else
-{
-	$pagetitle="VIM-LaTeX";
-	$leftpanel="left_main.inc";
-	$mainpanel="main.inc";
+else {
+	$mainpanel = "main.inc";
 }
-include "head.inc";
+if (isset($_GET['title'])) {
+	$pagetitle = $_GET['title'];
+}
+else {
+	$pagetitle = "";
+}
+include "head.inc"
 ?>
-
-
 <!-- End of left navigation bar. -->
 
 <td class="mainpanel">
